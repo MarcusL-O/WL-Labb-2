2421643cecb6eb4586c33d10a51c2c3b416361a9
MarcusLO00 <marcus.lisolivebring@iths.se>
2025-01-30 19:29:56 +0100

    CRUD Done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Wl_labb2.Data;
+using Wl_labb2.Models;
 
 namespace Wl_labb2
 {
@@ -19,11 +20,71 @@ namespace Wl_labb2
             //
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-            builder.Services.AddDbContext<AppDbContext> (options =>
-            options.UseSqlServer (connectionString));
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
 
 
             var app = builder.Build();
+
+            //CRUD
+
+            //Hämta alla snusprodukter
+            app.MapGet("/snus", async (AppDbContext db) =>
+            await db.SnusItems.ToListAsync());
+
+            //Hämta en specefik snus
+            app.MapGet("/snus/{id}", async (int id, AppDbContext db) =>
+            {
+                var snus = await db.SnusItems.FindAsync(id);
+
+                if (snus == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(snus);
+            });
+
+            //Skapa en specefik Snus
+            app.MapPost("/snus", async (Snus snus, AppDbContext db) =>
+            {
+                db.SnusItems.Add(snus);
+                await db.SaveChangesAsync();
+                return Results.Created($"/snus/{snus.Id}", snus);
+            });
+
+            //Uppdatera en snus
+            app.MapPut("/snus/{id}", async (int id, Snus updatedSnus, AppDbContext db) =>
+            {
+                var snus = await db.SnusItems.FindAsync(id);
+
+                if (snus == null)
+                {
+                    return Results.NotFound();
+                }
+
+                snus.Name = updatedSnus.Name;
+                snus.Description = updatedSnus.Description;
+                snus.Type = updatedSnus.Type;
+
+                await db.SaveChangesAsync();
+                return Results.NoContent(); 
+            });
+
+            //Deleta en snus
+            app.MapDelete("/snus/{id}", async (int id, AppDbContext db) =>
+            {
+                var snus = await db.SnusItems.FindAsync(id);
+
+                if (snus == null)
+                {
+                    return Results.NotFound();
+                }
+
+                db.SnusItems.Remove(snus);
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
